62a2ba6ed12c1cfc29444e1732ce63060c3a3f68
Yure Galdino <yuregaldino@hotmail.com>
2020-02-16 23:56:06 -0300

    Correçao do VMP

--- ProjAPA/VMP.py
+++ ProjAPA/VMP.py
@@ -1,36 +1,39 @@
-from Grafo import Grafo
+from CalculaRota import Calcula
+
 class VMP :
     def roteamento (self,grafo,capacidade,dimensao):
-        capacidade_total = 0
         ArrayRotas = []
-        cont_Rota = 0
         final = 0
-        while True :
+        while True:
             carga = 0;
             rota = []
             indice_atual = 0
             prox_indice = 0
-            while(carga+grafo.get_value()<= capacidade):
+            while(carga+grafo[indice_atual].get_value() <= capacidade):
                 menor_rota = 1000
                 aux_aresta = grafo[indice_atual].get_aresta()
                 for j in range(len(grafo[indice_atual].get_aresta())):
-                    if(indice_atual != j):
-                        if not (grafo[j].get_visitado() and aux_aresta[j]) < menor_rota:
-                            if ((carga + grafo[indice_atual].get_value() + grafo[j].get_value) <= capacidade):
+                    if indice_atual != j:
+                        if not grafo[j].get_visitado() and aux_aresta[j] < menor_rota:
+                            if ((carga + grafo[indice_atual].get_value() + grafo[j].get_value() ) <= capacidade):
                                 menor_rota = aux_aresta[j]
                                 prox_indice = j
-                                print(prox_indice)
-                            else:
-                                if (j == len(grafo[indice_atual].get_Aresta())):
-                                    menor_rota = aux_aresta[0]
+
                 if (grafo[indice_atual].get_id() != 0):
                     final += 1
-                    if(dimensao == final+1):
-                        menor_rota = aux_aresta[0]
-                    carga+= grafo[indice_atual].gel_value()
+
+                carga = carga + grafo[indice_atual].get_value()
+                if(grafo[indice_atual].get_id() != 0 ):
                     rota.append(grafo[indice_atual])
-                    grafo[indice_atual].set_visitado(True)
-                    indice_atual = prox_indice
+                grafo[indice_atual].set_visitado(True)
+                indice_atual = prox_indice
 
 
-        rotas_total.append(rota)
+            ArrayRotas.append(rota)
+            grafo[0].set_visitado(True)
+
+            if(dimensao == (final+1) ):
+                calculo = Calcula()
+                total_rota = calculo.calcula_rota(ArrayRotas)
+                return  ArrayRotas, total_rota
+
